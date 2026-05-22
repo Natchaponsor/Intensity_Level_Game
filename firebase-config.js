0ececc57f41eb74e3bcfdc9230adc939a99b4830
Natchapon (Top) Sortrakul <NATCHAPON.SOR@HOTMAIL.COM>
2026-05-22 18:09:21 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAuxxoHSL9OR88BG-c6C8I-Q5HJh4fuSnE",
+  authDomain: "on-a-scale-of-1-to-top.firebaseapp.com",
+  databaseURL: "https://on-a-scale-of-1-to-top-default-rtdb.firebaseio.com",
+  projectId: "on-a-scale-of-1-to-top",
+  storageBucket: "on-a-scale-of-1-to-top.firebasestorage.app",
+  messagingSenderId: "1071927649754",
+  appId: "1:1071927649754:web:293cefcb2feae100d3cce3",
+  measurementId: "G-TR6R0KF8CB"
+};
+
+export default firebaseConfig;
