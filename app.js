0b932cfdc3bd15fdc449aa5c29e634d4b7da3554
Natchapon (Top) Sortrakul <NATCHAPON.SOR@HOTMAIL.COM>
2026-05-22 18:18:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase, ref, set, get, update, onValue, push, remove }
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyAuxxoHSL9OR88BG-c6C8I-Q5HJh4fuSnE",
-  authDomain: "on-a-scale-of-1-to-top.firebaseapp.com",
-  databaseURL: "https://on-a-scale-of-1-to-top-default-rtdb.firebaseio.com",
-  projectId: "on-a-scale-of-1-to-top",
-  storageBucket: "on-a-scale-of-1-to-top.firebasestorage.app",
-  messagingSenderId: "1071927649754",
-  appId: "1:1071927649754:web:293cefcb2feae100d3cce3",
-  measurementId: "G-TR6R0KF8CB"
-};
 // ACTION_CARDS loaded from cards.json at startup (see initCards below)
 
 const firebaseConfig = {
